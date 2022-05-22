5d9c5d23c9aaf6db18f6532de887aa4c669f5566
Brent Aronsen <bkaronsen@gmail.com>
2022-05-22 16:34:55 -0500

    Start using express.js

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 // import core module into node.js. Our own file would need to start with ./ (relative) or / (absolute).
-const http = require('http');
-const routes = require('./routes');
+// const http = require('http'); -- no longer need this due to express.js
 
-const server = http.createServer(routes.handler); // use function exported (and imported here) in routes.js
+const express = require('express');
 
-// listen starts a process where node.js won't automatically close our script, but it will listen for requests
-server.listen(3000);
+const app = express(); // create express application
+
+app.use((req, res, next) => { // function will be executed for every incoming request
+  console.log('In the middleware!');
+  next(); // allows the request ot continue to the next middleware in line
+});
+
+app.use((req, res, next) => { // comes here from the previous function's next
+  console.log('In another middleware!');
+  res.send('<h1>Hello from Express!</h1>');
+});
+
+
+app.listen(3000); // calls http.createServer and calls listen on the created server
+// const server = http.createServer(app);
+
+// // listen starts a process where node.js won't automatically close our script, but it will listen for requests
+// server.listen(3000);
