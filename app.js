50004dec5c0fc55040be0683d008e835b01e719b
Brent Aronsen <bkaronsen@gmail.com>
2022-05-22 21:51:10 -0500

    Use body-parser

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express(); // create express application
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/favicon.ico', (req, res, next) => { // ignore favicon
   res.status(204).end();
 });
 
 app.use('/', (req, res, next) => {
-  console.log('This always runs!');
   next();
-})
+});
 
 app.use('/add-product', (req, res, next) => { // handles request
-  console.log('In add prouct middleware!');
-  res.send('<h1>Add product</h1>');
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 app.use('/', (req, res, next) => { // handles request
-  console.log('In another middleware!');
   res.send('<h1>Hello from Express!</h1>');
 });
 
