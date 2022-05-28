a7826c41e1887ef165d8814b6ccda15009e8bea4
Brent Aronsen <bkaronsen@gmail.com>
2022-05-28 18:17:11 -0500

    Add 404 controller

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,5 @@
-const path = require('path');
-
 const express = require('express');
 
-// const rootDir = require('../util/path');
 const productsController = require('../controllers/products');
 
 const router = express.Router();
