6700135b85efa448d565df3af1ce1ff468586453
Brent Aronsen <bkaronsen@gmail.com>
2022-05-30 11:45:59 -0500

    Get ready for dynamic routes

--- controllers/shop.js
+++ controllers/shop.js
@@ -24,14 +24,21 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
   res.render("shop/cart", {
-    pageTitle: "Your Cart",
     path: "/cart",
+    pageTitle: "Your Cart"
+  });
+};
+
+exports.getOrders = (req, res, next) => {
+  res.render("shop/orders", {
+    path: "/orders",
+    pageTitle: "Your Orders"
   });
 };
 
 exports.getCheckout = (req, res, next) => {
   res.render("shop/checkout", {
     path: "checkout",
-    pageTitle: "/checkout",
+    pageTitle: "/checkout"
   });
 };
